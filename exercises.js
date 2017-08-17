37f10f02373483d4a23792fd22aa255d69ae705c
Donnie Reese <donniereese@gmail.com>
2017-08-17 13:01:08 -0500

    Finished homework (homework.md) along with Feynman written, completed exercises and passed tests.  Uploading to github for submission.

--- exercises.js
+++ exercises.js
@@ -1,72 +1,156 @@
 // Do not change any of the function names
 
 function multiplyArguments() {
-  // use the arguments keyword to multiply all of the arguments together and return the product
-  // if no arguments are passed in return 0
-  // if one argument is passed in just return it
+    // use the arguments keyword to multiply all of the arguments together and return the product
+    // if no arguments are passed in return 0
+    // if one argument is passed in just return it
+    var total = 0;
+    for(var i = 0; i < arguments.length; i++) {
+        if( i === 0 ) {
+            total += arguments[i];
+        } else {
+            total *= arguments[i];
+        }
+    }
+
+    // arguments.reduce(function(total, val, index, arr){
+    //     total *= val;
+    //     if( index === 0 && arr.length === 1 )
+    //         total = val;
+    //     return ( index === 0 && arr.length === 1) ? val : total;
+    // }, 0);
+    //
+    return total;
 }
 
 function invokeCallback(cb) {
-  // invoke cb
+    // invoke cb
+    cb();
 }
 
 function sumArray(numbers, cb) {
-  // sum up all of the integers in the numbers array
-  // pass the result to cb
-  // no return is necessary
+    // sum up all of the integers in the numbers array
+    // pass the result to cb
+    // no return is necessary
+
+    /* forEach method, then pass to callback
+     * var total = 0;
+     * numbers.forEach(function(num){
+     *    total += num;
+     *});
+     *
+     * cb(total);
+     */
+
+    /* Reduce method. */
+    cb(numbers.reduce(function(sum, val, index, arr){
+        return sum + val;
+    }, 0));
+
 }
 
 function forEach(arr, cb) {
-  // iterate over arr and pass its values to cb one by one
-  // hint: you will be invoking cb multiple times (once for each value in the array)
+    // iterate over arr and pass its values to cb one by one
+    // hint: you will be invoking cb multiple times (once for each value in the array)
+    arr.forEach(function(val){
+        cb(val);
+    });
 }
 
 function map(arr, cb) {
-  // create a new array
-  // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
-  // the new array should be the same length as the array argument
+    // create a new array
+    // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
+    // the new array should be the same length as the array argument
+    var newArr = arr.map(function(val, i, a){
+        return cb(val);
+    });
+
+    return newArr;
 }
 
 function getUserConstructor() {
-  // create a constructor called User
-  // it should accept an options object with username, name, email, and password properties
-  // in the constructor set the username, name, email, and password properties
-  // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
-  // {{name}} should be the name set on each instance
-  // return the constructor
+    // create a constructor called User
+    // it should accept an options object with username, name, email, and password properties
+    // in the constructor set the username, name, email, and password properties
+    // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
+    // {{name}} should be the name set on each instance
+    // return the constructor
+    var User = function(opts) {
+        this.username = opts.username;
+        this.name = opts.name;
+        this.email = opts.email;
+        this.password = opts.password;
+
+        this.sayHi = function() {
+            return 'Hello, my name is ' + this.name;
+        };
+    };
+    return User;
 }
 
 function addPrototypeMethod(Constructor) {
-  // add a method to the constructor's prototype
-  // the method should be called 'sayHi' and should return the string 'Hello World!'
+    // add a method to the constructor's prototype
+    // the method should be called 'sayHi' and should return the string 'Hello World!'
+    Constructor.prototype.sayHi = function() {
+        return 'Hello World!';
+    };
+    return Constructor;
 }
 
 function addReverseString() {
-  // add a method to the string constructor's prototype that returns a reversed copy of the string
-  // name this method reverse
-  // hint:
-  // you will need to use 'this' inside of reverse
+    // add a method to the string constructor's prototype that returns a reversed copy of the string
+    // name this method reverse
+    // hint:
+    // you will need to use 'this' inside of reverse
+    String.prototype.reverse = function() {
+        return this.split('').reverse().join('');
+    };
 }
 
 function nFactorial(n) {
-  // return the factorial for n
-  // solve this recursively
-  // example:
-  // the factorial of 3 is 6 (3 * 2 * 1)
+    // return the factorial for n
+    // solve this recursively
+    // example:
+    // the factorial of 3 is 6 (3 * 2 * 1)
+    /* To use a for loop */
+    // var fact = 1;
+    // for(var i = n; i >= 1; i--) {
+    //     fact = fact * i;
+    // }
+    // return fact;
+
+    /* or to use recursion */
+    if(typeof this.fact === 'undefined') this.fact = 1;
+    if (n === 1) return this.fact * 1;
+
+    this.fact *= n;
+    return nFactoral(n - 1);
 }
 
 function cacheFunction(cb) {
-  // Extra Credit
-  // use closure to create a cache for the cb function
-  // the function that you return should accept a single argument and invoke cb with that argument
-  // when the function you return is invoked with an argument it should save that argument and its result
-  // when the function you return is called again with an argument that it has seen before it should not call cb
-  // but should instead directly returned the previous result
-  // example:
-  // cb -> function(x) { return x * x; }
-  // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
-  // if the function you return is invoked again with 5 it will look on an object in the closure scope
-  // and return 25 directly and will not invoke cb again
+    // Extra Credit
+    // use closure to create a cache for the cb function
+    // the function that you return should accept a single argument and invoke cb with that argument
+    // when the function you return is invoked with an argument it should save that argument and its result
+    // when the function you return is called again with an argument that it has seen before it should not call cb
+    // but should instead directly returned the previous result
+    // example:
+    // cb -> function(x) { return x * x; }
+    // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
+    // if the function you return is invoked again with 5 it will look on an object in the closure scope
+    // and return 25 directly and will not invoke cb again
+
+    var value;
+
+    return function(val) {
+
+        if( value != val ) {
+            value = val;
+            return cb(val);
+        } else {
+            return value;
+        }
+    };
 }
 
 
@@ -74,14 +158,14 @@ function cacheFunction(cb) {
 // --------------------------------
 
 module.exports = {
-  multiplyArguments: multiplyArguments,
-  invokeCallback: invokeCallback,
-  sumArray: sumArray,
-  forEach: forEach,
-  map: map,
-  getUserConstructor: getUserConstructor,
-  addPrototypeMethod: addPrototypeMethod,
-  addReverseString: addReverseString,
-  nFactorial: nFactorial,
-  cacheFunction: cacheFunction
+    multiplyArguments: multiplyArguments,
+    invokeCallback: invokeCallback,
+    sumArray: sumArray,
+    forEach: forEach,
+    map: map,
+    getUserConstructor: getUserConstructor,
+    addPrototypeMethod: addPrototypeMethod,
+    addReverseString: addReverseString,
+    nFactorial: nFactorial,
+    cacheFunction: cacheFunction
 };
---
+++ homework.md
@@ -0,0 +1,53 @@
+# Homework #4
+
+# 1. Feynman Writing Promps
+* Callback Functions
+    When we talk about functions, we are talking about saved instructions that we can use over
+    and over.  A Callback Function is another function, or another set of instructions, that we
+    can include in a function.  If you are making a cake, it may say something like "and use your
+    favorite icing recipe..."  When we follow our icing recipe, we are giving the result of our icing
+    recipe, the icing, and using it in our cake recipe.  Your cake recipe is the recipe you would be
+    refering to, or calling back to 'callback' when you are done with your icing recipe instructions!
+* Closure
+
+* `arguments`
+    `arguments` are the list of things that you give to a function when you use it.  In a cake recipe,
+    you would have to give all of your ingredients, add-ins and quantities to when you start your
+    cake recipe.  With a function, the `arguments` are information that our instructions in a function
+    need when it starts.
+* Recursion
+    Recursion is when a function or group of code has to call itself.  If you find yourself needing
+    to use the same instructions in your function on something else, instead of repeating yourself
+    over and over, you could just use your function again.  When making a cake, you might have a cake 
+    recipe.  Well, your recipe might give you the measurements for each layer of a cake and say "repeat
+    these instructions for every layer you want add."  The recipe wouldn't repeat itself, it would just
+    refer to the original recipe.
+
+* `prototype`
+    `prototype` is like the original of the original.  Code and the functions that come with it can be
+    copied.  If you copy something and modify it, then it's different.  If you want to modify something,
+    but it's so important that all of the different copies need what you are doing, then you could give
+    it a `prototype` function.  That gives it to the original and the original that everything is
+    copied from makes sure every version of it already has it. Every copy always has the `prototype`
+    functions that come with the original, even if it's a copy of a copy of a copy.
+* Constructors
+    Constructors are fancy functions.  It's like making a blueprint for something.  When you copy
+    that function, the **constructor**, it does some setup. It cleans house, makes sure all of the
+    functions and properties that it has are setup and can even take arguments and set things based
+    on what you give it.  You can use a Constructor to make multiple versions of something that all
+    have special functions and properties of their own.
+
+# 2. Fork and clone this repo. When you need to commit use the following commands
+    * `git status`
+    * `git add-all`
+    * `git status`
+    * `git commit -m "your commit message#
+    * `git push origin master`
+
+    **Done!**
+
+# 3. Install dependencies using `npm install`. Run tests using `npm test`.
+    **Done!**
+
+# 4. Make the tests pass!
+    **Done!**
